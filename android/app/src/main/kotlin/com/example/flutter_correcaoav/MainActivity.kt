f69df51665131f0597a2894b190973189931d415
giovana1806 <betareligiovana@gmail.com>
2024-05-23 11:36:33 -0300

    Flutter corrigindo erros do codigo

---
+++ android/app/src/main/kotlin/com/example/flutter_correcaoav/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_correcaoav
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_correcaoav/Principal.dart'; //nao estava errado, mudei para funcionar com o meu prjeto
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
+
